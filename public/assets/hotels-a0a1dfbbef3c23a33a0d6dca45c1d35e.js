3257534042dc413e9510d38246e0996cc7587fa4
sancho <sanchojaf@gmail.com>
2014-05-08 11:26:55 -0400

    clean assets

--- public/assets/hotels-a0a1dfbbef3c23a33a0d6dca45c1d35e.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {          
-  $('#datepicker1').datepicker({
-      format: "mm/dd/yyyy"
-  });  
-  $('#datepicker2').datepicker({
-      format: "mm/dd/yyyy"
-  });  
-});
